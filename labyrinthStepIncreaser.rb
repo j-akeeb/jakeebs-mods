9d873ee634aad577c6b3f85498dcc4eb3d15a920
jakeeb <knuckepic@gmail.com>
2025-08-22 20:58:56 -0400

    displays steps ty wire

--- labyrinthStepIncreaser.rb
+++ labyrinthStepIncreaser.rb
@@ -6,9 +6,10 @@ $WIRE_LATE_LOAD << proc {
     536 => 83, # Zorrialyn Labyrinth Ladder to Floor 3
     541 => 75 # Zorrialyn Labyrinth Dive Floor step setter
   }.each { |k, v| 
+
     InjectionHelper.defineMapPatch(k, v) { |event|
       event.patch(:zorrialynlabyrinthstepcount) { |page|
-        matched = event.lookForAll([:ControlVariable, :LabStepLimit, :Set, :Constant, nil])
+        matched = page.lookForAll([:ControlVariable, :LabStepLimit, :Set, :Constant, nil])
         for insn in matched
           insn.parameters[4] *= 5
         end
@@ -17,3 +18,47 @@ $WIRE_LATE_LOAD << proc {
     }
   }
 }
+  module ShowLabyrinthSteps
+    @@lastText = ''
+
+    def self.ensureBox
+      if !defined?(@@displaybox) || @@displaybox.contents.disposed?
+        @@displaybox = Window_UnformattedTextPokemon.new() # Unformatted for speed\
+        @@displaybox.setSkin("Graphics/Windowskins/choice 7")
+        positionBox
+        @@displaybox.z = 99999
+      end
+
+      @@displaybox.visible = [533, 536, 537, 541].include?($game_map.map_id)
+    end
+
+    def self.positionBox
+      @@displaybox.resizeToFit(@@displaybox.text,Graphics.width)
+      @@displaybox.x = 0
+      @@displaybox.y = Graphics.height - @@displaybox.height
+    end
+
+    def self.update
+      return if !$game_player || !$game_player
+      ensureBox
+      if @@displaybox.visible
+        position = "Steps: #{$game_variables[:LabStepLimit]}"
+
+        if @@lastText != position
+          @@lastText = position
+          @@displaybox.text=position
+          positionBox
+        end
+      end 
+    end
+  end
+
+  class Game_Screen
+    alias :showlabsteps_old_update :update
+
+    def update(*args, **kwargs)
+      ShowLabyrinthSteps.update
+      return showlabsteps_old_update(*args, **kwargs)
+    end
+  end
+  
