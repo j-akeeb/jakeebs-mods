b12b82a7728a5e413981192928773d1d42a476a9
jakeeb <knuckepic@gmail.com>
2025-10-25 19:25:58 -0400

    cool new fn thx wiresegal

--- betterMissy.rb
+++ betterMissy.rb
@@ -1,4 +1,10 @@
-def bettermissy_patchForm(species, form="Aevian Form", **values)
-  $cache.pkmn[species].formData[form].merge! values
+def bettermissy_patchForm(species, form=nil, **values)
+  if form.nil?
+    for key, value in values
+      $cache.pkmn[species].set_instance_variable("@#{key}".to_sym, value)
+    end
+  else
+    $cache.pkmn[species].formData[form].merge! values
+  end
 end # expandable if i feel like it
-bettermissy_patchForm(:MISMAGIUS, BaseStats: [60,105,105,60,60,105])
+bettermissy_patchForm(:MISMAGIUS, BaseStats: [75,105,90,60,60,105], Abilities: [:MAGICBOUNCE, :TECHNICIAN, :TANGLINGHAIR])
